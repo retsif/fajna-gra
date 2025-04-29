11439425e1216f013dc957691621704903e084fc
Piasiuu <126461336+Piasiuu@users.noreply.github.com>
2025-04-30 00:13:56 +0200

    Delete godot project directory

--- godot project/sceny/platforma.gd
+++
@@ -1,4 +0,0 @@
-extends AnimatableBody2D
-
-func _ready():
-	$AnimationPlayer.play("ruch")
--- godot project/sceny/poziom1.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://sceny/glowne_menu.tscn")
